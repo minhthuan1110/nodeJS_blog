3d8212e67108eeca9c93162f36958f92f01905a5
thuan <minthuantn1110@gmail.com>
2024-08-28 11:02:52 +0700

    starting...

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello word ')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    console.log('xin chao ban yeu')
+})
